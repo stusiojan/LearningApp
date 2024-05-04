f32b61eff16ddf428f72cf7e3a3a6853132a5ee7
Kacper Kowalczyk <kacper.kowalczyk4.stud@pw.edu.pl>
2024-05-04 15:24:49 +0200

    Add redundant columns to category and milestone

--- src/main/java/main/lib/Category.java
+++ src/main/java/main/lib/Category.java
@@ -8,11 +8,17 @@ public class Category {
     private final String name;
     private final String description; // nullable
 
+    private final int tasksAll;
+
+    private final int tasksDone;
+
     public Category(ResultSet row) {
         try {
             id = row.getInt("cat_id");
             name = row.getString("cat_name");
             description = row.getString("cat_description");
+            tasksAll = row.getInt("cat_tasks_all");
+            tasksDone = row.getInt("cat_tasks_done");
         } catch (SQLException ex) {
             throw new RuntimeException("Creating object from database row failed: " + ex.getMessage());
         }
@@ -28,5 +34,13 @@ public class Category {
 
     public String getDescription() {
         return description;
+    }
+
+    public int getTasksAll() {
+        return tasksAll;
+    }
+
+    public int getTasksDone() {
+        return tasksDone;
     }
 }
