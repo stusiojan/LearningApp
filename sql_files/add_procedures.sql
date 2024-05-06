f3769e876b2bb57d45975f778836753f06de9389
Kacper Kowalczyk <kacper.kowalczyk4.stud@pw.edu.pl>
2024-05-06 11:29:18 +0200

    fix triggers, add first test data

--- sql_files/add_procedures.sql
+++ sql_files/add_procedures.sql
@@ -30,70 +30,66 @@ CREATE OR REPLACE FUNCTION task_count_week    RETURN tasks.task_id%TYPE AS BEGIN
 CREATE OR REPLACE FUNCTION task_count_month   RETURN tasks.task_id%TYPE AS BEGIN RETURN task_count(ADD_MONTHS(sysdate, 1)); END;
 /
 
-CREATE OR REPLACE PROCEDURE update_completion (arg_id tasks.task_id%TYPE)
+CREATE OR REPLACE PROCEDURE update_completion (arg_id milestones.mil_id%TYPE)
 AS
-    mst_id        milestones.mil_id%TYPE;
     mst_old_done  milestones.mil_tasks_done%TYPE;
     mst_old_count milestones.mil_tasks_all%TYPE;
     mst_completed milestones.mil_completed%TYPE;
 BEGIN
-    SELECT mil_id, mil_tasks_done, mil_tasks_all, mil_completed
-    INTO   mst_id, mst_old_done,   mst_old_count, mst_completed
-    FROM   tasks NATURAL JOIN milestones
-    WHERE  task_id = arg_id;
+    SELECT mil_tasks_done, mil_tasks_all, mil_completed
+    INTO   mst_old_done,   mst_old_count, mst_completed
+    FROM   milestones
+    WHERE  mil_id = arg_id;
     
     IF mst_old_done  = mst_old_count AND mst_completed IS     NULL THEN
-        UPDATE milestones SET mil_completed = sysdate WHERE mil_id = mst_id;
+        UPDATE milestones SET mil_completed = sysdate WHERE mil_id = arg_id;
     END IF;
     IF mst_old_done != mst_old_count AND mst_completed IS NOT NULL THEN
-        UPDATE milestones SET mil_completed = NULL    WHERE mil_id = mst_id;
+        UPDATE milestones SET mil_completed = NULL    WHERE mil_id = arg_id;
     END IF;
-    update_completion(arg_id);
 END;
 /
 
 CREATE OR REPLACE TRIGGER tg_new_task
 AFTER INSERT ON tasks FOR EACH ROW
 DECLARE
-    mst_id        milestones.mil_id%TYPE;
     mst_old_count milestones.mil_tasks_all%TYPE;
     ctg_id        categories.cat_id%TYPE;
     ctg_old_count categories.cat_tasks_all%TYPE;
 BEGIN
-    SELECT mil_id, mil_tasks_all, cat_id, cat_tasks_all
-    INTO   mst_id, mst_old_count, ctg_id, ctg_old_count
-    FROM   tasks NATURAL JOIN milestones NATURAL JOIN categories
-    WHERE  task_id = :new.task_id;
+    SELECT mil_tasks_all, cat_id, cat_tasks_all
+    INTO   mst_old_count, ctg_id, ctg_old_count
+    FROM   milestones NATURAL JOIN categories
+    WHERE  mil_id = :new.mil_id;
     
-    UPDATE milestones SET mil_tasks_all = 1 + mst_old_count WHERE mil_id = mst_id;
+    UPDATE milestones SET mil_tasks_all = 1 + mst_old_count WHERE mil_id = :new.mil_id;
     UPDATE categories SET cat_tasks_all = 1 + ctg_old_count WHERE cat_id = ctg_id;
-    update_completion(:new.task_id);
+    update_completion(:new.mil_id);
 END;
 /
 
 CREATE OR REPLACE TRIGGER tg_del_task
 BEFORE DELETE ON tasks FOR EACH ROW
 DECLARE
-    mst_id        milestones.mil_id%TYPE;
     mst_old_done  milestones.mil_tasks_done%TYPE;
     mst_old_count milestones.mil_tasks_all%TYPE;
     ctg_id        categories.cat_id%TYPE;
     ctg_old_done  categories.cat_tasks_done%TYPE;
     ctg_old_count categories.cat_tasks_all%TYPE;
 BEGIN
-    SELECT mil_id, mil_tasks_done, mil_tasks_all, cat_id, cat_tasks_done, cat_tasks_all
-    INTO   mst_id, mst_old_done,   mst_old_count, ctg_id, ctg_old_done,   ctg_old_count
-    FROM   tasks NATURAL JOIN milestones NATURAL JOIN categories
-    WHERE  task_id = :old.task_id;
+    SELECT mil_tasks_done, mil_tasks_all, cat_id, cat_tasks_done, cat_tasks_all
+    INTO   mst_old_done,   mst_old_count, ctg_id, ctg_old_done,   ctg_old_count
+    FROM   milestones NATURAL JOIN categories
+    WHERE  mil_id = :old.mil_id;
     
-    UPDATE milestones SET mil_tasks_all = mst_old_count - 1 WHERE mil_id = mst_id;
+    UPDATE milestones SET mil_tasks_all = mst_old_count - 1 WHERE mil_id = :old.mil_id;
     UPDATE categories SET cat_tasks_all = ctg_old_count - 1 WHERE cat_id = ctg_id;
     
     IF :old.task_completed IS NOT NULL THEN
-        UPDATE milestones SET mil_tasks_done = mst_old_done - 1 WHERE mil_id = mst_id;
+        UPDATE milestones SET mil_tasks_done = mst_old_done - 1 WHERE mil_id = :old.mil_id;
         UPDATE categories SET cat_tasks_done = ctg_old_done - 1 WHERE cat_id = ctg_id;
     END IF;
-    update_completion(:old.task_id);
+    update_completion(:old.mil_id);
 END;
 /
 
@@ -121,7 +117,7 @@ BEGIN
         UPDATE milestones SET mil_tasks_done = mst_old_done - 1 WHERE mil_id = mst_id;
         UPDATE categories SET cat_tasks_done = ctg_old_done - 1 WHERE cat_id = ctg_id;
     END IF;
-    update_completion(arg_id);
+    update_completion(mst_id);
 END;
 /
 
--- sql_files/test_data.sql
+++ sql_files/test_data.sql
@@ -1,8 +1,32 @@
-ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY/MM/DD';
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY/MM/DD HH24:MI:SS';
 
-INSERT INTO categories (cat_name, cat_description) VALUES ('Bazy Danych 2', 'Projektowanie aplikacji z bazami danych przy użyciu zaawansowanych mechanizmów');
-INSERT INTO users (login, hash, salt) VALUES ('Kacper', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'AAAAAAAAAAAAAAAA');
-INSERT INTO milestones (mil_name, date_added, deadline, mil_completed, mil_description, user_id, cat_id) VALUES ('Model fizyczny', sysdate, TO_DATE('2024/05/20'), sysdate, 'Zrobić resztę', 1, 1);
-INSERT INTO tasks (task_name, task_completed, task_description, mil_id) VALUES ('Postawienie bazy danych', NULL, 'Napisać zapytania tworzące bazę danych i wstawiające minimalną ilość danych', 1);
+INSERT INTO users (login, hash, salt) VALUES ('user1', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', '12345678abcdefgh');
+    INSERT INTO categories (cat_name, cat_description) VALUES ('Niemiecki', 'Język niemiecki');
+    INSERT INTO categories (cat_name, cat_description) VALUES ('Python', NULL);
+        INSERT INTO milestones (mil_name, date_added, deadline, mil_completed, mil_description, user_id, cat_id) VALUES ('Podstawy języka', TO_DATE('2024/02/02'), TO_DATE('2024/03/02'), TO_DATE('2024/02/28'), 'Najważniejsze elementy języka', 1, 2);
+            INSERT INTO tasks (task_name, task_completed, task_description, mil_id) VALUES ('Typy danych', TO_DATE('2024/02/04'), 'Ciągi znaków, liczby całkowite i zmiennoprzecinkowe', 1);
+            INSERT INTO tasks (task_name, task_completed, task_description, mil_id) VALUES ('Pętle', TO_DATE('2024/02/11'), 'Pętle for i while', 1);
+            INSERT INTO tasks (task_name, task_completed, task_description, mil_id) VALUES ('Instrukcje warunkowe', TO_DATE('2024/02/28'), NULL, 1);      
+        INSERT INTO milestones (mil_name, date_added, deadline, mil_completed, mil_description, user_id, cat_id) VALUES ('Programowanie obiektowe', sysdate, sysdate, sysdate, '', 1, 2);
+
+INSERT INTO users (login, hash, salt) VALUES ('user2', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', '1234567890123456');
+
+
+INSERT INTO users (login, hash, salt) VALUES ('user3', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'a1b2c3d4e5f6g7h8');
+    INSERT INTO categories (cat_name, cat_description) VALUES ('Bazy Danych 2', 'Projektowanie aplikacji z bazami danych przy użyciu zaawansowanych mechanizmów');
+        INSERT INTO milestones (mil_name, date_added, deadline, mil_completed, mil_description, user_id, cat_id) VALUES ('Egzamin', sysdate, TO_DATE('2024/06/17'), TO_DATE('2024/03/02 21:24:20'), 'Egzamin - wszystkie wykłady', 3, 1);
+            INSERT INTO tasks (task_name, task_completed, task_description, mil_id) VALUES ('Wykład - wprowadzenie', TO_DATE('2024/03/01 20:17:34'), NULL, 3);
+            INSERT INTO tasks (task_name, task_completed, task_description, mil_id) VALUES ('Wykład - model pojęciowy', TO_DATE('2024/03/01 20:17:38'), NULL, 3);
+            INSERT INTO tasks (task_name, task_completed, task_description, mil_id) VALUES ('Wykład - model relacyjny', TO_DATE('2024/03/01 20:17:43'), NULL, 3);
+            INSERT INTO tasks (task_name, task_completed, task_description, mil_id) VALUES ('Wykład - hurtownie danych', TO_DATE('2024/03/02 21:24:02'), NULL, 3);
+            INSERT INTO tasks (task_name, task_completed, task_description, mil_id) VALUES ('Wykład - model fizyczny', TO_DATE('2024/03/02 21:24:13'), NULL, 3);
+            INSERT INTO tasks (task_name, task_completed, task_description, mil_id) VALUES ('Wykład - NoSQL', TO_DATE('2024/03/02 21:24:20'), NULL, 3);
+        INSERT INTO milestones (mil_name, date_added, deadline, mil_completed, mil_description, user_id, cat_id) VALUES ('Projekt', sysdate, TO_DATE('2024/05/20'), sysdate, 'Zrobić resztę', 3, 1);
+    INSERT INTO categories (cat_name, cat_description) VALUES ('Sztuka wytwarzania oprogramowania', 'Poszerzanie wiedzy umożliwiającej pisanie lepszego kodu');
+    INSERT INTO categories (cat_name, cat_description) VALUES ('Systemy komputerowe w sterowaniu i pomiarach', 'Praca z mikrokontrolerami i systemami czasu rzeczywistego');
+    INSERT INTO categories (cat_name, cat_description) VALUES ('Wstęp do multimediów', 'Informacje z zakresu sygnałów, kodowania, wizji, dźwięku i obrazu');
+    INSERT INTO categories (cat_name, cat_description) VALUES ('Wprowadzenie do systemów zarządzania', 'Systemy zarządzania, programowanie liniowe oraz modelowanie procesów biznesowych i harmonogramów');
+    INSERT INTO categories (cat_name, cat_description) VALUES ('Bezpieczeństwo systemów i sieci', 'Informacje z zakresu kryptografii i cyberbezpieczeństwa');
+
 
 COMMIT;
